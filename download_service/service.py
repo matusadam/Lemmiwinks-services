16a01c9d332877c1653c14cde4ed769a6c478487
Adam Matus <adam.matus@seznam.cz>
2018-11-20 20:32:47 +0100

    changed intent to spaces

--- download_service/service.py
+++ download_service/service.py
@@ -1,37 +1,41 @@
 from sanic import Sanic
 from sanic import response
 
-
-
 import aiohttp
 
 app = Sanic()
 
 async def get(session, url):
-	try:
-		async with session.get(url) as resp:
-			responseData = await resp.text()
-			return {
-				'index': {
-					"data": responseData,
-					"type": resp.content_type,
-				},
-			}
-	except Exception as e:
-		return {
-			'error' : str(e),
-			'url' : url,
-		}
+    try:
+        async with session.get(url) as resp:
+            responseData = await resp.text()
+            return {
+                "status" : 200,
+                "data": responseData,
+                "type": resp.content_type,
+                'url' : url,
+            }
+    except Exception as e:
+        return {
+            'status' : 500,
+            'error' : str(e),
+            'url' : url,
+        }
+
+async def make_response(request):
+    url = request.json.get("url")
+    if url:
+        async with aiohttp.ClientSession() as session:
+            resp = await get(session, url=url)
+            return response.json(resp)
+    else:
+        return response.json(dict())
+
 
 @app.route('/', methods=['POST'])
 async def post_handler(request):
-	url = request.json.get("url")
-	if url:
-		async with aiohttp.ClientSession() as session:
-			resp = await get(session, url=url)
-			return response.json(resp)
-	else:
-		return response.json(dict())
+    return await make_response(request)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port="8081")
