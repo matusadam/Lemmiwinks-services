ef72b3debb6ce0afb2c7020821e1d01bfa138c73
matusadam <adam.matus@seznam.cz>
2019-06-16 12:47:40 +0200

    Added get archive route, direct download

--- archive_service/curl_test.sh
+++ archive_service/curl_test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 curl -i -X POST -H 'Content-Type: application/json' -d '{"urls": ["http://www.example.org"]}' http://0.0.0.0:8080/archive
+
+curl -i -X GET http://0.0.0.0:8080/archive/20190616T122434_P1N4FJZ9.maff
+
+curl -i -X POST -H 'Content-Type: application/json' -d '{"urls": ["http://3g2upl4pq6kufc4m.onion/"]}' http://0.0.0.0:8080/archive
--- archive_service/service.py
+++ archive_service/service.py
@@ -1,13 +1,6 @@
-# import asyncio
-# import aiohttp
 
-# import lemmiwinks
-# import lemmiwinks.taskwrapper as taskwrapper
-# import lemmiwinks.archive.migration as migration
-# import lemmiwinks.httplib as httplib
-# import lemmiwinks.pathgen as pathgen
-# import lemmiwinks.parslib as parslib
-# import lemmiwinks.archive as archive
+import os
+import re
 
 from schema import ArchiveJsonSchema
 from name_generator import ArchiveName
@@ -31,6 +24,35 @@ async def post_archive_handler(request):
 @app.route('/archive', methods=['GET'])
 async def get_archive_handler(request):
     return response.html(await archive_get(request))
+
+@app.route('/archive/<name>', methods=['GET'])
+async def get_archive_download_handler(request, name):
+    # match archive name
+    m = re.match(r'[0-9]+T[0-9]+_[0-9A-Z]+\.maff', name)
+    if not m:
+        return response.json(
+            {
+                "error": "Bad request: archive name does not match",
+                "status" : 400
+            },
+            status=400
+        )
+
+    # return maff if exists
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        if name in f:
+            return await response.file(f)
+
+    # archive doesnt exist
+    return response.json(
+            {
+                "error": "Archive %s not found" % (name,),
+                "status" : 404
+            },
+            status=404
+        )
+
 
 @app.route('/', methods=['GET'])
 async def get_root_handler(request):
